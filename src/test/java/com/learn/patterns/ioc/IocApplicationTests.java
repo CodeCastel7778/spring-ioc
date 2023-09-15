82ed3a2ec4c2c32f60ad9eeeb259b66fd90a8dca
Jayanth Kumar <j.ei7778@gmail.com>
2023-09-15 13:16:04 +0530

    Initial commit

---
+++ src/test/java/com/learn/patterns/ioc/IocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.patterns.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
